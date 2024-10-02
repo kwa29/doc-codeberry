2963baf241a3c3e3d858c397e88ab0678ef33f9b
Didier Caroff <djkwa29@gmail.com>
2024-10-02 23:10:42 +0200

    GOGOGO

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,21 +1,9 @@
 export default {
   logo: <span>CodeBerry Docs</span>,
   project: {
-<<<<<<< HEAD
     link: 'https://github.com/kwa29/docs-codeberry'
   },
   docsRepositoryBase: 'https://github.com/kwa29/docs-codeberry/tree/main',
-=======
-<<<<<<< Updated upstream
-    link: 'https://github.com/yourusername/codeberry-docs'
-  },
-  docsRepositoryBase: 'https://github.com/yourusername/codeberry-docs/tree/main',
-=======
-    link: 'https://github.com/kwa29/docs-codeberry'
-  },
-  docsRepositoryBase: 'https://github.com/kwa29/docs-codeberry/tree/main',
->>>>>>> Stashed changes
->>>>>>> fe7e4b1 (Live)
   footer: {
     text: 'CodeBerry Documentation'
   }
