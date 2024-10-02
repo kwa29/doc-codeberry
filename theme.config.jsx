8409c8c93885a223aafd54547445e69650649617
Didier Caroff <djkwa29@gmail.com>
2024-10-02 21:56:37 +0200

    First Doc

---
+++ theme.config.jsx
@@ -0,0 +1,10 @@
+export default {
+  logo: <span>CodeBerry Docs</span>,
+  project: {
+    link: 'https://github.com/kwa29/docs-codeberry'
+  },
+  docsRepositoryBase: 'https://github.com/kwa29/docs-codeberry/tree/main',
+  footer: {
+    text: 'CodeBerry Documentation'
+  }
+}
